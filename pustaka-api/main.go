026c68078e8d7837e45200255a761bce35d15247
ynw99 <yusufedu99@gmail.com>
2021-10-15 14:13:08 +0700

    intro gin

--- pustaka-api/main.go
+++ pustaka-api/main.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import (
+	"net/http"
 
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"name": "Yusuf Nur Wahid",
+			"bio":  "A Software Engineer",
+		})
+	})
+
+	router.GET("/hello", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"title":    "Hello World!",
+			"subtitle": "Belajar Golang bareng Agung Setiawan",
+		})
+	})
+
+	router.Run(":8888")
 }
