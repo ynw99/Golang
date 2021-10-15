b5e2df1043e7fffcc99fd9197c20becb6f71844a
ynw99 <yusufedu99@gmail.com>
2021-10-15 14:57:43 +0700

    path variable and query string

--- pustaka-api/main.go
+++ pustaka-api/main.go
@@ -20,12 +20,31 @@ func helloHandler(c *gin.Context) {
 	})
 }
 
+func booksHandler(c *gin.Context) {
+	id := c.Param("id")
+	title := c.Param("title")
+	c.JSON(http.StatusOK, gin.H{
+		"id":    id,
+		"title": title,
+	})
+}
+
+func queryHandler(c *gin.Context) {
+	title := c.Query("title")
+	price := c.Query("price")
+	c.JSON(http.StatusOK, gin.H{
+		"title": title,
+		"price": price,
+	})
+}
+
 func main() {
 	router := gin.Default()
 
 	router.GET("/", rootHandler)
-
 	router.GET("/hello", helloHandler)
+	router.GET("/books/:id/:title", booksHandler)
+	router.GET("/query", queryHandler)
 
 	router.Run(":8080")
 }
