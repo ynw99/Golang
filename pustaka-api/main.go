457227b7abc68abb3ddb83082a4b2c23e140b8e1
ynw99 <yusufedu99@gmail.com>
2021-10-18 15:06:20 +0700

    mengambil data

--- pustaka-api/main.go
+++ pustaka-api/main.go
@@ -20,21 +20,69 @@ func main() {
 		log.Fatal("DB Connection Error")
 	}
 
-	db.AutoMigrate(&book.Book{})
+	// db.AutoMigrate(&book.Book{})
 
 	// CRUD
-	book := book.Book{}
-	book.Title = "Atomic Habits"
-	book.Price = 120000
-	book.Discount = 15
-	book.Rating = 4
-	book.Description = "Buku self development tentang membangun kebiasaan baik dan menghilangkan kebiasaan buruk"
 
-	err = db.Create(&book).Error
+	// Create
+	/*
+		book := book.Book{}
+		book.Title = "Atomic Habits"
+		book.Price = 120000
+		book.Discount = 15
+		book.Rating = 4
+		book.Description = "Buku self development tentang membangun kebiasaan baik dan menghilangkan kebiasaan buruk"
+
+		err = db.Create(&book).Error
+		if err != nil {
+			fmt.Println("==========================")
+			fmt.Println("ERROR CREATING BOOK RECORD")
+			fmt.Println("==========================")
+		}
+	*/
+
+	// Read
+	// var book book.Book
+
+	// Slice of books
+	var books []book.Book
+
+	// First row
+	// err = db.First(&book).Error
+
+	// First row - primary key
+	// err = db.First(&book, 2).Error
+
+	// Last row
+	// err = db.Last(&book).Error
+
+	// Show query first
+	// err = db.Debug().First(&book).Error
+
+	// Show query last
+	// err = db.Debug().Last(&book).Error
+
+	// Retrieve many data objects
+	// err = db.Debug().Find(&books).Error
+
+	// Retrieve many data by string conditions
+	err = db.Debug().Where("rating <> ?", 4).Find(&books).Error
+
 	if err != nil {
 		fmt.Println("==========================")
-		fmt.Println("ERROR CREATING BOOK RECORD")
+		fmt.Println("ERROR FINDING BOOK RECORD")
 		fmt.Println("==========================")
+	}
+
+	// fmt.Println("Title:", book.Title)
+	// fmt.Println("Price:", book.Price)
+	// fmt.Println("Description:", book.Description)
+	// fmt.Println("Book Object:", book)
+
+	// Iterate through books slice
+	for _, b := range books {
+		fmt.Println("Title:", b.Title)
+		fmt.Println("Objek:", b)
 	}
 
 	router := gin.Default()
