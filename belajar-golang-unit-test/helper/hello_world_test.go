a8167678135db84122995d8883dc6c0de2d9b1e4
ynw99 <yusufedu99@gmail.com>
2021-10-21 12:48:07 +0700

    assertion

--- belajar-golang-unit-test/helper/hello_world_test.go
+++ belajar-golang-unit-test/helper/hello_world_test.go
@@ -3,7 +3,22 @@ package helper
 import (
 	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
+
+func TestHelloWorldRequire(t *testing.T) {
+	result := HelloWorld("Yusuf")
+	require.Equal(t, "Hello Yusuf", result, "Result must be Hello Yusuf")
+	fmt.Println("TestHelloWorld with Require Done")
+}
+
+func TestHelloWorldAssert(t *testing.T) {
+	result := HelloWorld("Yusuf")
+	assert.Equal(t, "Hello Yusuf", result, "Result must be Hello Yusuf")
+	fmt.Println("TestHelloWorld with Assert Done")
+}
 
 func TestHelloWorldYusuf(t *testing.T) {
 	result := HelloWorld("Yusuf")
