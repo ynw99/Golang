6a9e91aa1e3aa8f55e8fd6fccd57a7ce96d4c487
ynw99 <yusufedu99@gmail.com>
2021-10-22 07:16:49 +0700

    mock testing

--- belajar-golang-unit-test/service/category_service_test.go
+++ belajar-golang-unit-test/service/category_service_test.go
@@ -1,15 +1,37 @@
 package service
 
 import (
+	"belajar-golang-unit-test/entity"
 	"belajar-golang-unit-test/repository"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 )
 
 var categoryRepository = &repository.CategoryRepositoryMock{Mock: mock.Mock{}}
 var categoryService = CategoryService{Repository: categoryRepository}
 
-func TestCategoryService_Get(t *testing.T) {
+func TestCategoryService_GetNotFound(t *testing.T) {
 	// Program Mock
+	categoryRepository.Mock.On("FindByID", "1").Return(nil)
+
+	category, err := categoryService.Get("1")
+	assert.Nil(t, category)
+	assert.NotNil(t, err)
+}
+
+func TestCategoryService_GetSucceed(t *testing.T) {
+	category := entity.Category{
+		Id:   "2",
+		Name: "laptop",
+	}
+
+	categoryRepository.Mock.On("FindByID", "2").Return(category)
+
+	result, err := categoryService.Get("2")
+	assert.Nil(t, err)
+	assert.NotNil(t, category)
+	assert.Equal(t, category.Id, result.Id)
+	assert.Equal(t, category.Name, result.Name)
 }
